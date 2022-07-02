82f89a2b904a5d98e2ddfb9a4b1dc11667705dd4
Yusuf <Yusuf@DESKTOP-O7K9VTG>
2022-07-02 17:04:59 +0300

    Add project files.

---
+++ MauiApp7/GraphicsDrawable.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiApp7
+{
+    public class GraphicsDrawable : IDrawable
+    {
+        public string Text { get; set; }
+        public void Draw(ICanvas canvas, RectF dirtyRect)
+        {
+            canvas.DrawString(Text, 60, 60, HorizontalAlignment.Center);
+        }
+    }
+}
---
+++ MauiApp7/MainPage.xaml.cs
@@ -0,0 +1,19 @@
+﻿namespace MauiApp7;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        var draw = new GraphicsDrawable
+        {
+            Text = "Hello"
+        };
+        GraphicsDrawable drawable = new GraphicsDrawable();
+        graphicsView.Invalidate();
+    }
+}
